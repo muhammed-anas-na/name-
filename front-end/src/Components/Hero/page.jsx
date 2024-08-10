2f3be39fcd697291bbd234fb5ac8679f10154a43
Anas <anasna6005@gmail.com>
2024-08-10 12:39:38 +0530

    Banner01 added

---
+++ front-end/src/Components/Hero/page.jsx
@@ -0,0 +1,18 @@
+import styles from'./page.module.css'
+
+function Hero() {
+  return (
+    <div className={styles.container}>
+        <div className="relative">
+            <img className='absolute inset-0 w-full h-96 object-cover' src="banner01.jpg" alt="banner"/>
+            <h1
+            className=
+            'relative inset-0 flex top-40 justify-center text-white font-extrabold sm:text-3xl lg:text-5xl'>
+              Destination Weddings
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@/Components/Header/page';
-import Commingsoon from '../Components/ComingSoon/Page';
+import Hero from '@/Components/Hero/page';
+// import Commingsoon from '../Components/ComingSoon/Page';
 export default function Home() {
   return (
     <main>
-      {/* <Commingsoon/> */}
       <Header/>
+      <Hero/>
     </main>
   );
 }
