f9132266052422e57987cc6834e488a4f6e1f01d
Anas <anasna6005@gmail.com>
2024-08-10 22:33:18 +0530

    Category list added

--- front-end/src/Components/Hero/Hero.jsx
+++ front-end/src/Components/Hero/Hero.jsx
@@ -9,8 +9,7 @@ function Hero() {
           <img
             className="w-full h-96 object-cover"
             src="banner01.jpg"
-            alt="banner"
-          />
+            alt="banner01"          />
           <h1 className="absolute inset-0 flex top-40 justify-center text-white font-extrabold text-2xl sm:text-4xl lg:text-5xl">
             Destination Weddings
           </h1>
@@ -20,23 +19,23 @@ function Hero() {
         <div className="md:flex md:mt-5 gap-5">
           <div className="relative">
             <img
-            className="w-full object-cover"
-            src="banner02.jpg"
-            alt="banner"
+              className="w-full object-cover"
+              src="banner02.jpg"
+              alt="banner"
             />
             <h1 className="absolute inset-0 flex top-28 lg:top-44 justify-center text-white font-extrabold text-2xl lg:text-4xl">
-            Photography
-          </h1>
+              Photography
+            </h1>
           </div>
           <div className="relative">
-          <img
-            className="w-full object-cover"
-            src="banner03.jpg"
-            alt="banner"
+            <img
+              className="w-full object-cover"
+              src="banner03.jpg"
+              alt="banner"
             />
             <h1 className="absolute inset-0 flex top-28 lg:top-52 justify-center text-white font-extrabold text-2xl lg:text-4xl">
-            Dress Guide
-          </h1>
+              Dress Guide
+            </h1>
           </div>
         </div>
       </div>
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,11 +1,13 @@
+import Category from '@/Components/CategoryList/Category';
 import Header from '@/Components/Header/page';
-import Hero from '@/Components/Hero/page';
+import Hero from '@/Components/Hero/Hero';
 // import Commingsoon from '../Components/ComingSoon/Page';
 export default function Home() {
   return (
     <main>
       <Header/>
       <Hero/>
+      <Category/>
     </main>
   );
 }
