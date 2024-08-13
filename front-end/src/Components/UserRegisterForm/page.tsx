500c6b87efc5a908ed58c406f2ae4e7e0b7dea5f
Anas <anasna6005@gmail.com>
2024-08-14 00:19:05 +0530

    user register page done

--- front-end/src/Components/UserRegisterForm/page.tsx
+++ front-end/src/Components/UserRegisterForm/page.tsx
@@ -1,41 +1,41 @@
 "use client";
 
 import { useState, useRef } from "react";
+import React from "react";
 
-function UserLoginForm() {
-  const [email, setEmail] = useState("");
-  const [number, setNumber] = useState();
-  const [password, setPassword] = useState("");
-  const emailRef = useRef<HTMLInputElement>(null);
+function UserRegisterForm() {
+  const [number, setNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const numberRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="flex flex-col border border-gray-300 shadow-xl p-3 w-80">
-        <h1 className="font-bold my-3">Login Form</h1>
+        <h1 className="font-bold my-3">Register Form</h1>
         <div className="flex justify-center items-center">
           <h1 className="border-4 border-pink-500 rounded-full p-2 px-4">1</h1>
           <div className="border w-40 h-0"></div>
           <h1
             className={`border-2 ${
-              !email ? "border-black" : "border-pink-500 border-4"
+              !number ? "border-black" : "border-pink-500 border-4"
             } rounded-full p-2 px-4`}
           >
             2
           </h1>
         </div>
-        {!email ? (
+        {!number ? (
           <div className="my-4">
-            <h1>Email</h1>
+            <h1>Number</h1>
             <input
-              ref={emailRef}
-              placeholder="Enter email"
+              ref={numberRef}
+              placeholder="Enter Number"
               type="text"
               className="border border-gray-500 focus:outline-none h-10 w-full"
             />
             <button
               className="bg-pink-500 text-white font-bold py-2 mt-2 w-full"
               onClick={() => {
-                if (emailRef.current) setEmail(emailRef.current.value);
+                if (numberRef.current) setNumber(numberRef.current.value);
               }}
             >
               Next
@@ -43,21 +43,21 @@ function UserLoginForm() {
           </div>
         ) : (
           <div className="my-4">
-            <h1>Password</h1>
+            <h1>OTP</h1>
             <div className="flex items-center">
               <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter password"
-                type="password"
-                className="border border-gray-500 border-e-0 focus:outline-none h-10 w-full"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter OTP"
+                type="number"
+                className="border border-gray-500 border-dashed focus:outline-none h-10 w-full"
               />
-              <button className="border h-10 border-gray-500 border-s-0">Show</button>
             </div>
+            <h1 className="text-gray-500 text-xs text-end my-3 hover:cursor-pointer">Resend OTP?</h1>
             <button
-              className="bg-pink-500 text-white font-bold py-2 mt-2 w-full"
+              className="bg-pink-500 text-white font-bold py-2 mt-2 w-full justify-end"
             >
-              Login
+              Register
             </button>
           </div>
         )}
@@ -66,4 +66,4 @@ function UserLoginForm() {
   );
 }
 
-export default UserLoginForm;
+export default UserRegisterForm;
---
+++ front-end/src/app/userRegister/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserRegisterForm from '../../Components/UserRegisterForm/page';
+
+function userRegister(){
+    return(
+        <UserRegisterForm/>
+    )
+}
+
+export default userRegister;
